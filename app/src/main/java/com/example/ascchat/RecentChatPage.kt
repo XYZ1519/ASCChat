249a5282c955779f80382df0f9b5b70daa7beeda
Naphat Far <78838224+naphatamity@users.noreply.github.com>
2022-11-04 16:16:25 +0700

    add recent chat page

--- app/src/main/java/com/example/ascchat/RecentChatPage.kt
+++ app/src/main/java/com/example/ascchat/RecentChatPage.kt
@@ -2,28 +2,75 @@ package com.example.ascchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.amity.socialcloud.sdk.chat.AmityChatClient
 import com.amity.socialcloud.sdk.chat.channel.AmityChannel
+import com.amity.socialcloud.sdk.chat.channel.AmityChannelFilter
 import com.amity.socialcloud.sdk.chat.channel.AmityChannelRepository
 import com.amity.socialcloud.sdk.core.AmityTags
+import com.example.ascchat.chatadapter.ChatAdapter
+import com.example.ascchat.chatadapter.ListListener
 import com.google.gson.JsonObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class RecentChatPage : AppCompatActivity() {
+class RecentChatPage : AppCompatActivity(), ListListener {
+    var chatAdapter: ChatAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recent_chat_page)
         findViewById<TextView>(R.id.channelID).text = "UserId1"
-
+        initChatFragment()
         initChannelRepository()
 
     }
 
-    fun initChannelRepository() {
-        val channelRepository = AmityChatClient.newChannelRepository()
-        createConversationChannel(channelRepository)
+    private fun initChatFragment() {
+        val recycler = findViewById<RecyclerView>(R.id.channelListRecycler)
+        chatAdapter = ChatAdapter(this)
+        recycler.apply {
+            layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+            isNestedScrollingEnabled = false
+            adapter = chatAdapter
+            onFlingListener = null
+        }
     }
 
+    fun initChannelRepository() {
+        val channelRepository = AmityChatClient.newChannelRepository()
+        initChannel(channelRepository)
+       // createConversationChannel(channelRepository)
+    }
+
+    private fun initChannel(channelRepository: AmityChannelRepository) {
+        val channelQuery = getChannelCollection(channelRepository)
+        channelQuery.observe(this, Observer {
+            if ((chatAdapter?.itemCount ?: Int.MAX_VALUE) < it.size) {
+                chatAdapter?.submitList(it)
+            }
+        })
+    }
+
+
+    private fun getChannelCollection(channelRepository: AmityChannelRepository): LiveData<PagedList<AmityChannel>> {
+        return LiveDataReactiveStreams.fromPublisher(
+            channelRepository.getChannels()
+                .all()
+                .filter(AmityChannelFilter.MEMBER)
+                .build()
+                .query()
+                .subscribeOn(Schedulers.io())
+                .observeOn((AndroidSchedulers.mainThread()))
+        )
+    }
 
     fun createConversationChannel(channelRepository: AmityChannelRepository) {
         // create channel and let SDK handle channelId generation
@@ -39,5 +86,9 @@ class RecentChatPage : AppCompatActivity() {
                 //success
             }
             .subscribe()
+    }
+
+    override fun onItemClick(chatItem: AmityChannel, position: Int, holder: View) {
+        Log.e("onClick", chatItem.getChannelId())
     }
 }
---
+++ app/src/main/java/com/example/ascchat/chatadapter/ChatViewHolder.kt
@@ -0,0 +1,35 @@
+package com.example.ascchat.chatadapter
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.amity.socialcloud.sdk.AmityCoreClient
+import com.amity.socialcloud.sdk.chat.channel.AmityChannel
+import com.example.ascchat.R
+
+class ChatViewHolder(itemsView: View) : RecyclerView.ViewHolder(itemsView) {
+    var userRepository = AmityCoreClient.newUserRepository()
+
+    fun bind(item: AmityChannel) {
+        itemView.apply {
+            addMessageToView(this, item)
+        }
+    }
+
+    private fun addMessageToView(view: View, item: AmityChannel) {
+        view.findViewById<TextView>(R.id.chId).text = item.getDisplayName()
+        item.getMetadata()?.let { metadataObject ->
+            if(metadataObject.getAsJsonArray("USER_IDS") != null) {
+                if(metadataObject.getAsJsonArray("USER_IDS").size() > 1) {
+                    metadataObject.getAsJsonArray("USER_IDS").map {
+                        if (!it.asString.equals(
+                                userRepository.getCurrentUser().blockingFirst().getDisplayName()
+                            )
+                        )
+                        view.findViewById<TextView>(R.id.chId).text = it.asString
+                    }
+                }
+            }
+        }
+    }
+}
